909308a92f4f79ff5dbb5f7f4916290a0df2da72
r122338 yamaji <r122338@g.kobe-kosen.ac.jp>
2025-08-19 16:24:44 +0900

    動作部分はできました。 サーバ側をいじるのが難しそうならこっちをいじります。

---
+++ demo/src/main/java/com/example/demo/TimeController.java
@@ -0,0 +1,30 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.ZonedDateTime;
+
+@RestController
+public class TimeController {
+
+    @GetMapping("/Apoint")
+    public String getCurrentTime() {
+        ZonedDateTime now = ZonedDateTime.now();
+        int hour = now.getHour();
+        int min = now.getMinute();
+        switch (hour){
+            case 11,12,13,14,15,16:
+                if(min==55){
+                    return "「巡回時刻は5分前です。次の巡回場所は○○です。」";
+                    }
+                    if(min==0){
+                    return "「巡回時刻になりました。巡回場所は○○です。」";
+                }
+                    else return "成功" ;
+            default:
+            return "対応時間外です";
+        }
+    }
+}
+
