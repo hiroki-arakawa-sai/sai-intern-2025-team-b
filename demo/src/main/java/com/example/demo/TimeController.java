f09252bf005bf0af1b8edf5a154d3fc7ecbaf9b2
r122338 yamaji <r122338@g.kobe-kosen.ac.jp>
2025-08-20 11:08:47 +0900

    現段階です。関数を実行するようにするとか、データベースとかから引っ張ってくるとか、jSON形式で書かないといけないとかになったらまたここを書き換えてください。

--- demo/src/main/java/com/example/demo/TimeController.java
+++ demo/src/main/java/com/example/demo/TimeController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.time.ZonedDateTime;
 
+
 @RestController
 public class TimeController {
 
@@ -14,17 +15,47 @@ public class TimeController {
         int hour = now.getHour();
         int min = now.getMinute();
         switch (hour){
-            case 11,12,13,14,15,16:
+            case 10,13:
                 if(min==55){
-                    return "「巡回時刻は5分前です。次の巡回場所は○○です。」";//
+                    return "「巡回時刻は5分前です。次の巡回場所は2階の食品テナントです。」";//
                     }
                     if(min==0){
-                    return "「巡回時刻になりました。巡回場所は○○です。」";
+                    return "「巡回時刻になりました。巡回場所は1階の食品テナントです。」";
                 }
-                    else return "成功" ;
+                    else return "" ;
+            case 11,14:
+                if(min==20){
+                    return "「巡回時刻は5分前です。次の巡回場所は3階の駐車場です。」";//
+                    }
+                    if(min==0){
+                    return "「巡回時刻になりました。巡回場所は2階の食品テナントです。」";
+                }
+                    else return "" ;
+            case 12,15:
+            if(min==20){
+                    return "「巡回時刻は5分前です。次の巡回場所はすべての階層です。」";//
+                    }
+                    if(min==0){
+                    return "「巡回時刻になりました。巡回場所は3階の食品テナントです。」";
+                }
+                    else return "" ;
+            case 16:
+                    if(min==0){
+                    return "「巡回時刻になりました。巡回場所はすべての階層です。」";
+                }
+                    else return "" ;
             default:
             return "対応時間外です";
         }
     }
+
+    @GetMapping("/recode")
+    public String getrecode() {
+        
+
+        return new String();
+    }
+
+    
 }
 
