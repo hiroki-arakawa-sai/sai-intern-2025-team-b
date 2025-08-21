52ad6103afa953ef2942f530459390c489caac05
GitHubharuya <2hareruya1016@gmail.com>
2025-08-21 11:12:01 +0900

    レコード.java

---
+++ demo/src/main/java/com/example/demo/Record.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+public class Record{
+  private int year,month,date,hour,min;
+  private String place,memo;
+
+  public Record(String rec){
+    for(int i=0; i<rec.length(); i++){
+      
+    }
+  }
+}
