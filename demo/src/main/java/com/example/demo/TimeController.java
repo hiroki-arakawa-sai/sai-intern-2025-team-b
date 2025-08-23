f4ec196665beab0f219f4b82923c36929671b014
ItsukiArakaki268 <itukiprog@gmail.com>
2025-08-23 17:25:41 +0900

    TImeController修正

--- demo/src/main/java/com/example/demo/TimeController.java
+++ demo/src/main/java/com/example/demo/TimeController.java
@@ -20,18 +20,18 @@ public class TimeController {
         int hour = now.getHour();
         int min = now.getMinute();
         switch (min){
-            case 55:
+            case 31:
                 if(hour==9 || hour==12){
-                    bbca.call("「巡回時刻は5分前です。次の巡回場所は1階の食品売り場です。」");
+                    bbca.call("「巡回時刻の5分前です。次の巡回場所は1階の食品売り場です。」");
                 }
                 else if(hour==10 || hour==13){
-                    bbca.call("「巡回時刻は5分前です。次の巡回場所は2階のテナントです。」");
+                    bbca.call("「巡回時刻の5分前です。次の巡回場所は2階のテナントです。」");
                 }
                 else if(hour==11 || hour==14){
-                    bbca.call("「巡回時刻は5分前です。次の巡回場所は3階の駐車場です。」");
+                    bbca.call("「巡回時刻の5分前です。次の巡回場所は3階の駐車場です。」");
                 }
                 else if(hour==15){
-                    bbca.call("「巡回時刻は5分前です。次の巡回場所は全階です。」");
+                    bbca.call("「巡回時刻の5分前です。次の巡回場所は全階です。」");
                 }
             case 0:
                 if(hour==10 || hour==13){
@@ -48,12 +48,6 @@ public class TimeController {
                 }
             default: bbca.call("「対応時間外です」");
         }
-    }
-
-    public String getrecode() {
-        
-
-        return new String();
     }
 }
 
